63c999a02d8774a3d00bc4059838268691af515e
Vidyarana10 <vidhyarana10@gmail.com>
2024-04-18 18:24:51 +0530

    vidhya Added

--- AAA/src/com/AAA/A.java
+++ AAA/src/com/AAA/A.java
@@ -4,6 +4,9 @@ public class A {
 	public void m1() {
 		System.out.println("hello everyone");
 	}
+	public  void m2() {
+		System.out.println("Good Morning");
+	}
 
 	public static void main(String[] args) {
 		
